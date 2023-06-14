9f050a9987cbd29867c8593696eeba9ad2590884
leonardo2322 <joys23services@gmail.com>
2023-06-13 23:37:41 -0400

    proyecto de tareas terminado

--- lista_de_tareas/urls.py
+++ lista_de_tareas/urls.py
@@ -5,6 +5,12 @@ urlpatterns = [
     path('home/',views.home, name='home'),
     path('signup/', views.signup, name='signup'),
     path('tasks/',views.tasks, name='tasks'),
+    path('tasks_completed/',views.completed_task, name='tasks_completed'),
     path('logout/', views.signout, name='logout'),
-    path('signin/',views.signin, name='signin')
+    path('signin/',views.signin, name='signin'),
+    path('create/task/', views.create_Task, name='create_task'),
+    path('tasks/<int:task_id>/complete_task',views.complete, name='complete_task'),
+    path('tasks/<int:task_id>/',views.TaskDetail, name='taskdetail'),
+    path('tasks/<int:task_id>/delete',views.delete_task, name='deletetask')
+    
 ]
--- lista_de_tareas/views.py
+++ lista_de_tareas/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.contrib.auth import login, logout, authenticate
+from .forms import CreateTasks
+from .models import Task
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def signup(request):
     if request.method == 'GET':
@@ -20,19 +24,26 @@ def signup(request):
             except:
                 return HttpResponse('el usuario ya existe')
         return HttpResponse('Incorrrect password')
-
-
+@login_required
+def Errors(request,error):
+    return render(request, 'errors.html',{
+        'ERROR': 'Error in the server please verify what have you done ' ,
+        'desc_error': error
+    })
+@login_required
 def home(request):
     return render(request, 'home.html')
-
+@login_required
 def tasks(resquest):
-    return render(resquest, 'tasks.html')
+    Tasks = Task.objects.filter(user=resquest.user, dateCompleted__isnull = True)
+    return render(resquest, 'tasks.html',{'todo_Tasks': Tasks})
 
 def signout(request):
     logout(request)
-    return redirect('home')
+    return redirect('/')
 
 def signin(request):
+    
     if request.method == 'GET':
         return render(request, 'signin.html',{
             'form':AuthenticationForm
@@ -49,3 +60,58 @@ def signin(request):
         else:
             login(request, user)
             return redirect('tasks')
+@login_required    
+def create_Task(request):
+
+    if request.method == 'GET':
+        return render(request, 'create_task.html',{
+            'form': CreateTasks
+        })
+    else:
+        try:
+
+            form = CreateTasks(request.POST)
+            new_task = form.save(commit=False)
+            new_task.user = request.user
+            new_task.save()
+            return redirect('tasks')
+        except ValueError:
+            return Errors(request)
+@login_required       
+def TaskDetail(request,task_id):
+    print(request.user,'aqui')
+    if request.method == 'GET':
+        try:
+            task = get_object_or_404(Task, pk=task_id, user=request.user)
+            form =CreateTasks(instance=task)
+            return render(request,'task_detail.html',{'task_id':task,'form':form})
+        except ValueError:
+            return Errors(request,'error')
+    else:
+        try:
+
+            task = get_object_or_404(Task, pk=task_id, user=request.user)
+            form = CreateTasks(request.POST,instance=task)
+            form.save()
+            return redirect('tasks')
+        except:
+
+            return Errors(request,'error 404')
+@login_required       
+def complete(request,task_id):
+    task = get_object_or_404(Task, pk=task_id, user=request.user)
+        
+    if request.method == 'POST':
+            task.dateCompleted = timezone.now()
+            task.save()
+            return redirect('tasks')
+@login_required 
+def delete_task(request,task_id):
+    task = get_object_or_404(Task, pk=task_id, user= request.user)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('tasks')
+@login_required
+def completed_task(request):
+    task = Task.objects.filter(user=request.user, dateCompleted__isnull = False).order_by('-dateCompleted')
+    return render(request,'tasks.html', {'todo_Tasks':task})
